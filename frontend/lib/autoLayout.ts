8393c41050a7ed2017c809f2b5a58e93dc8239a6
gabrielribeirogpx-arch <gabrielribeirogpx@gmail.com>
2026-04-20 19:59:37 -0300

    feat(flow-builder): apply automatic dagre node layout

---
+++ frontend/lib/autoLayout.ts
@@ -0,0 +1,43 @@
+import dagre from 'dagre';
+import type { Edge, Node } from 'reactflow';
+
+const nodeWidth = 260;
+const nodeHeight = 140;
+
+export function getLayoutedElements(nodes: Node[], edges: Edge[]) {
+  const dagreGraph = new dagre.graphlib.Graph();
+  dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+  dagreGraph.setGraph({
+    rankdir: 'LR',
+    nodesep: 80,
+    ranksep: 120,
+  });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, {
+      width: nodeWidth,
+      height: nodeHeight,
+    });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  const layoutedNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+
+    return {
+      ...node,
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
+    };
+  });
+
+  return { nodes: layoutedNodes, edges };
+}
